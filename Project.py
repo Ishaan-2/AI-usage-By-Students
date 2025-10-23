e210d0212ae70e5eb8608b5bc0301752d33e5c7b
Ishaan-2 <sehg.ishaan05@gmail.com>
2025-10-23 15:42:01 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -7,8 +7,9 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #uploading files
-uploaded_file=st.file_uploader('Cleaned',type='csv')
-df = uploaded_file
+uploaded_file = st.file_uploader('Cleaned',type='csv')
+express_gbp = pd.read_csv(uploaded_file)
+df = express_gbp
     
 st.set_page_config(page_title='AI Usage')
 st.title('AI USAGE BY STUDENTS :streamlit:')
@@ -75,6 +76,7 @@ with tab4:
      fig_6=px.pie(fdf,names='Willing_to_Pay_for_Access',hole=0.4,title='Students Willing to Pay for Access')
 
      st.plotly_chart(fig_6)
+
 
 
 
